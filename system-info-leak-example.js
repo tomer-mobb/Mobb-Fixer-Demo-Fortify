53d033fad38d00ca1fd469014d28f23adf66f009
tomer-mobb <132216976+tomer-mobb@users.noreply.github.com>
2024-01-17 15:37:57 +0700

    Update system-info-leak-example.js

--- system-info-leak-example.js
+++ system-info-leak-example.js
@@ -1,60 +1,10 @@
 const http = require('node:http');
 const fs = require('node:fs/promises');
-const {createReadStream} = require('node:fs');
-
-function errorHandler(e) {
-    console.error(e);
-}
-
-function logError() {
-    console.error(...arguments);
-}
 
 http.createServer((req, res) => {
     fs.readFile("./no-file.txt").catch((e) => {
         console.error(e);
     });
-
-    fs.readFile("./no-file.txt").catch((e) => {
-        logError(e);
-    });
-
-    fs.readFile("./no-file.txt").catch(({message}) => {
-        console.error(message);
-    });
-
-    fs.readFile("./no-file.txt").catch((e) => console.error(e));
-
-    fs.readFile("./no-file.txt").catch(function (e) {
-        console.error(e.toString());
-    });
-
-    fs.readFile("./no-file.txt").catch(function (e) {
-        return console.error(e.toString());
-    });
-
-    fs.readFile("./no-file.txt").catch(errorHandler);
-
-    createReadStream("./no-file.txt").on('error', function (error) {
-        console.log(error.message);
-    });
-
-    createReadStream("./no-file.txt").on('error', error => console.log(error.message));
-
-    (async () => {
-        try {
-            await fs.readFile("./no-file.txt");
-        } catch (e) {
-            console.error(e);
-        }
-
-        try {
-            await fs.readFile("./no-file.txt");
-        } catch (e) {
-            return console.error(e);
-        }
-    })();
-
     res.writeHead(200);
     res.end("👋");
 }).listen(8080);
